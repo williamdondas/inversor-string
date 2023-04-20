58fe92bf026fba82140c83aaea1f208dca53ba5d
williamdondas <williamdondas@hotmail.com>
2023-04-20 00:48:05 -0300

    adicionado inversor de strings

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function inverter(string){
+    var saida = ''
+    for(var i = string.length-1;i>=0;i--){
+        saida+= string[i]
+        
+    }
+    console.log(saida);        
+}
+
+inverter('william')
+
